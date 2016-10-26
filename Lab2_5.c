eb5cbde5540bd444f47fac0c06f8e72aa78ddec9
Chih-Chao Chang <cchan113@jhu.edu>
2016-10-25 23:23:07 -0400

    Create Lab2_5.c

---
+++ Lab2_5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	
+	float z;
+	printf("Enter a float number: ");
+	scanf("%f",&z);
+	int s=z+0.5;
+	printf("The number after rounding is: %d",s);
+	
+
+
+//	system("pause");
+	return 0;
+	
+}
