b5fc6416f0e6424eae7c7fcac9c1e468d72e5414
Chih-Chao Chang <cchan113@jhu.edu>
2016-10-25 23:21:39 -0400

    Create lab2_2.c

---
+++ lab2_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+	
+
+	int year=2014,month=9,day=24,hour=7,minute=31,second=8; 
+	printf("Date:%.2d\\%.2d\\%.2d\n",year,month,day);
+	printf("Time:%.2d:%.2d:%.2d\n",hour,minute,second);
+	
+		return 0;
+	
+}
